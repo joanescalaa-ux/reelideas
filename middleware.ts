a109cb71ddf515059e000aee09295575a3454e42
Joan Escala <joanescalaa@gmail.com>
2026-06-02 03:12:36 +0200

    fix: middleware auth

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,5 @@
-import { auth } from './auth';
-
-export default auth((req) => {
-  const isLoggedIn = !!req.auth;
-  const isLoginPage = req.nextUrl.pathname === '/login';
-  const isRegisterPage = req.nextUrl.pathname === '/register';
-
-  if (!isLoggedIn && !isLoginPage && !isRegisterPage) {
-    return Response.redirect(new URL('/login', req.nextUrl));
-  }
-
-  if (isLoggedIn && (isLoginPage || isRegisterPage)) {
-    return Response.redirect(new URL('/', req.nextUrl));
-  }
-});
+export default function middleware() {}
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|_next/data).*)'],
+  matcher: [],
 };
